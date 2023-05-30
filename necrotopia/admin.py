9e2d3865a5133aeca5764640a9067533aaba6033
Eiredrake <eiredrake@gmail.com>
2023-05-30 12:26:53 -0400

    moved users into custom admin group

--- necrotopia/admin.py
+++ necrotopia/admin.py
@@ -1,33 +1,57 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
+from necrotopia.forms import RegisterUserForm
 from necrotopia.models import UserProfile
+from django.contrib.auth.models import Group as DjangoGroup
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.utils.translation import gettext_lazy as _translate
 from necrotopia_project.settings import GLOBAL_SITE_NAME
-#
-#
-# class CustomUserAdmin(UserAdmin):
-#     add_form = CustomUserCreationForm
-#     form = CustomUserChangeForm
-#
-#     model = UserProfile
-#
-#     list_display = ('email', 'is_active',
-#                     'is_staff', 'is_superuser', 'last_login',)
-#     list_filter = ('is_active', 'is_staff', 'is_superuser')
-#     fieldsets = (
-#         (None, {'fields': ('email', 'password')}),
-#         ('Permissions', {'fields': ('is_staff', 'is_active',
-#                                     'is_superuser', 'groups', 'user_permissions')}),
-#         ('Dates', {'fields': ('last_login', 'date_joined')})
-#     )
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
-#          ),
-#     )
-#     search_fields = ('email',)
-#     ordering = ('email',)
-#
-#
-# admin.site.register(UserProfile, CustomUserAdmin)
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = RegisterUserForm
+    form = RegisterUserForm
+
+    model = UserProfile
+
+    list_display = ('email', 'is_active',
+                    'is_staff', 'is_superuser', 'last_login',)
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active',
+                                    'is_superuser', 'groups', 'user_permissions')}),
+        ('Dates', {'fields': ('last_login', 'date_joined')})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(UserProfile, CustomUserAdmin)
+
+
+class Group(DjangoGroup):
+    """Instead of trying to get new user under existing `Aunthentication and Authorization`
+    banner, create a proxy group model under our Accounts app label.
+    Refer to: https://github.com/tmm/django-username-email/blob/master/cuser/admin.py
+    """
+
+    class Meta:
+        verbose_name = _translate('group')
+        verbose_name_plural = _translate('groups')
+        proxy = True
+
+
+admin.site.unregister(DjangoGroup)
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin):
+    pass
